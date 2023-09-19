aff7e678807a7ee6168dc76f0ad3cb7dc5d0205f
Saidur Rahaman Shaon <shaonsimt@gmail.com>
2023-09-19 22:37:57 +0600

    new feature added

--- script.js
+++ script.js
@@ -32,21 +32,47 @@ depositBtn.addEventListener('click', function(){
 
     document.getElementById("depositAmount").value = "";
 
+    // increase Balance event handler
 
+    const totalBalance = document.getElementById("totalBalance").innerText;
 
+    const totalBalanceNumber = parseFloat(totalBalance);
 
+    const newTotalBalance = depositNumber + totalBalanceNumber;
 
-
-
-
-
-    
-
-
-    
-
+    document.getElementById("totalBalance").innerText = newTotalBalance;
 
 })
+
+// Withdraw Button event handler 
+
+var withdrawBtn = document.getElementById("addWithdraw");
+
+withdrawBtn.addEventListener('click', function(){
+
+const withdrawAmount = document.getElementById('withdrawAmount').value;
+const withdrawAmountNumber = parseFloat(withdrawAmount);
+
+const totalWithdraw = document.getElementById('totalWithdraw').innerText;
+const totalWithdrawNumber = parseFloat(totalWithdraw);
+
+const newTotalWithdraw = withdrawAmountNumber + totalWithdrawNumber;
+
+document.getElementById('totalWithdraw').innerText = newTotalWithdraw;
+
+document.getElementById('withdrawAmount').value = "";
+
+// decrease Balance event handler
+
+const totalBalance = document.getElementById("totalBalance").innerText;
+
+    const totalBalanceNumber = parseFloat(totalBalance);
+    const newTotalBalance = totalBalanceNumber - withdrawAmountNumber;
+    document.getElementById("totalBalance").innerText = newTotalBalance;
+
+})
+
+
 
 
 
